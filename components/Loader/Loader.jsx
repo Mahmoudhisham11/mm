202aa8d79feae976eb73b7b0d27bb989719dd5f1
Mahmoud Hisham <mahmoudhisham11@gmail.com>
2025-12-12 01:34:26 +0200

    Add new files and updates

--- components/Loader/Loader.jsx
+++ components/Loader/Loader.jsx
@@ -2,7 +2,7 @@
 import styles from "./styles.module.css";
 export default function Loader() {
   return (
-    <div className={stylse.loaderContainer}>
+    <div className={styles.loaderContainer}>
       <p className={styles.loader}>
         <span>Double M</span>
       </p>
